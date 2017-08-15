a7c578bd9ccda316eebe5d9aa7fc3ef6c5742467
Andy Bitz <artzbitz13+git@gmail.com>
2017-08-15 20:17:34 +0200

    Added texts

--- components/texts.js
+++ components/texts.js
@@ -1,34 +1,194 @@
 export default [
+  
+  // Alas, nothing happend.
   <span>
-    hello world
-    <style jsx>
-    {`
-      color: red;
-    `}
-    </style>
+    Alas, nothing happend.
   </span>,
+
+  // HK, do you know what love is?
   <span>
-    hello world
-    <style jsx>
-    {`
-      color: green;
-    `}
-    </style>
+    HK, do you know what love is?
   </span>,
+
+  // Tuturuuuu, Okarin
   <span>
-    hello world
-    <style jsx>
-    {`
-      color: hotpink;
-    `}
-    </style>
+    Tuturuuuu, Okarin
   </span>,
+
+  // Cheeki Breeki
+  <span>Cheeki Breeki</span>,
+
+  // STAIRS???!! noOOOOOOOooooooOOoooOoooo!!!!
+  <span>STAIRS???!! noOOOOOOOooooooOOoooOoooo!!!!</span>,
+
+  // If anyone tries to capture me, I'll incinerate their brain.
   <span>
-    hello world
-    <style jsx>
-    {`
-      color: grey;
-    `}
-    </style>
+    If anyone tries to capture me, I'll incinerate their brain.
   </span>,
+
+  // Git Gud
+  <span>
+    Git Gud
+  </span>,
+
+  // YOU DIED
+  <span>
+    YOU DIED
+  </span>,
+
+  // hello ^•^/
+  <span>
+    hello ^•^/
+  </span>,
+
+  // <3
+  <span>
+    &lt;3
+  </span>,
+
+  // 今日は
+  <span>
+    今日は
+  </span>,
+
+  // F12
+  <span>
+    F12
+  </span>,
+
+  // >del /S /Q C:\\windows\\system32\\
+  <span>
+    &gt;del /S /Q C:\\windows\\system32\\
+  </span>,
+
+  // Outdated, destroy society.
+  <span>
+    Outdated, destroy society.
+  </span>,
+
+  // We are not retreating, we are advancing in reverse.
+  <span>
+    We are not retreating, we are advancing in reverse.
+  </span>,
+
+  // Wololo
+  <span>
+    Wololo
+  </span>,
+
+  // durararararararara
+  <span>
+    durararararararara
+  </span>,
+
+  // 0xdeadbeef
+  <span>
+    0xdeadbeef
+  </span>,
+
+  // >git commit suicide - Thats how you delete a repo.
+  <span>
+    >git commit suicide Thats how you delete a repo.
+  </span>,
+
+  // ニャアアアアアアアアアアアア
+  <span>
+    ニャアアアアアアアアアアアア
+  </span>,
+
+  // KOTOR > The Movies - my opinion
+  <span>
+    KOTOR > The Movies - my opinion
+  </span>,
+
+  // I wonder if I plant you in the ground, if you will grow taller?
+  <span>
+    I wonder if I plant you in the ground, if you will grow taller?
+  </span>,
+
+  // Здравствуйте! // Zdravstvuyte!
+  <span>
+    Здравствуйте!
+  </span>,
+
+  // Slytherin or Ravenclaw - please, no Hufflepuff
+  <span>
+    Slytherin or Ravenclaw - please, no Hufflepuff
+  </span>,
+
+  // IZZZZAAAAAAYYYYYY&#8203;AAAAAAAAAAAA!!!!
+  <span>
+    IZZZZAAAAAAYYYYYYAAAAAAAAAAAA!!!!
+  </span>,
+
+  // Clown Prince of Crime
+  <span>
+    Clown Prince of Crime
+  </span>,
+
+  // RULES OF NATURE
+  <span>
+    RULES OF NATURE
+  </span>,
+
+  // METAL GEAR?!
+  <span>  
+    METAL GEAR?!
+  </span>,
+
+  // Chimichangas!
+  <span>
+    Chimichangas!
+  </span>,
+
+  // We did it reddit.
+  <span>
+    We did it reddit.
+  </span>,
+
+  // This is not meth.
+  <span>
+    This is not meth.
+  </span>,
+
+  // Hi. What do you play? - Wow, um. Zelda, Tetris... - ... That's kind of a big question.
+  <span>
+    Hi. What do you play? - Wow, um. Zelda, Tetris... - ... That's kind of a big question.
+  </span>,
+
+  // segmentation fault
+  <span>
+    segmentation fault
+  </span>,
+
+  // xor rax, rax
+  <span>
+    xor rax, rax
+  </span>,
+
+  // Flowers, Butterflies and Dragons.
+  <span>
+    Flowers, Butterflies and Dragons.
+  </span>,
+
+  // carry me senpai
+  <span>
+    carry me senpai
+  </span>,
+
+  // *nodebleed*
+  <span>*nosebleed*</span>,
+
+  // shizuo chan ^•^ 
+  <span>
+    shizuo chan ^•^
+  </span>,
+
+  // 最高に, 最高に, 最高に君達が大好きだ
+  <span>
+    最高に, 最高に, 最高に君達が大好きだ
+  </span>,
+
+  // Click the picture.
+  <span>Click the picture.</span>
 ]
