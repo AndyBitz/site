caf242576aba955f9f67edf9604250c7716153a0
Andy Bitz <artzbitz@gmail.com>
2018-01-10 22:42:34 +0100

    Added Google Tag Manager with Google Analytics

--- layouts/page.js
+++ layouts/page.js
@@ -1,5 +1,15 @@
 import Head from 'next/head'
 import Meta from '../components/meta'
+import Router from 'next/router'
+
+// handle router events in google analytics
+Router.onRouteChangeStart = (url) => {
+  try {
+    const tracker = window.ga.getAll()[0]
+    tracker.set('page', url)
+    tracker.send('pageview')
+  } catch(err) {}
+}
 
 export default ({ title, children }) => {
   title = title || "hello ^•^/"
@@ -9,7 +19,9 @@ export default ({ title, children }) => {
       <Meta title={title} />
       <Head>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NQV94X');`}}></script>
       </Head>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQV94X" height="0" width="0" style={{visbilit: 'hidden', display: 'none' }}></iframe></noscript>
       <style jsx>
       {`
         main {
