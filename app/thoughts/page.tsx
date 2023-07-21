99b2155d48986096b7b3689c520bbc85060084a9
Andy Bitz <artzbitz@gmail.com>
2023-07-21 18:42:30 +0200

    Use Ronin

--- app/thoughts/page.tsx
+++ app/thoughts/page.tsx
@@ -1,49 +1,36 @@
+import ronin from 'ronin';
+import type { Thoughts } from '@ronin/andybitz';
 import { Link } from "../components/link";
 import styles from './page.module.css';
 
-async function getPosts() {
-	return {
-		posts: [
-			{
-				title: 'Borrowing: An important thing about Rust',
-				postedAt: new Date('2018-08-01T00:00:00.000Z').getTime(),
-				externalLink: 'https://medium.com/@AndyBitz/borrowing-an-important-thing-about-rust-3e19c563bf35',
-			},
-			{
-				title: 'On making my first browser-online-multiplayer-game',
-				postedAt: new Date('2018-04-11T00:00:00.000Z').getTime(),
-				externalLink: 'https://medium.com/@AndyBitz/on-making-my-first-browser-online-multiplayer-game-f0fbce2c9564',
-			}
-		],
-	};
-}
-
 export default async function Thoughts() {
-	const { posts } = await getPosts();
+	const [thoughts] = await ronin<Thoughts>(({ get }) => {
+		get.thoughts.orderedBy!.descending = ['postedAt'];
+	});
 
 	return (
 		<div>
 			<ul className={styles.thoughtsList}>
-				{posts.map(post => {
+				{thoughts.map(thought => {
 					return (
 						<li>
 							<h3>
-								<Link href={post.externalLink} target="_blank">
-									{post.title}
+								<Link href={thought.externalLink} target="_blank">
+									{thought.title}
 								</Link>
 							</h3>
 							<div className={styles.thoughtsListSub}>
 								<span>
-									{new Date(post.postedAt).toISOString().split('T')[0]}
+									{new Date(thought.postedAt).toISOString().split('T')[0]}
 								</span>
-								{post.externalLink ? (
+								{thought.externalLink ? (
 									<span>
 										&#8212;on Medium
 									</span>
 								) : null}
 							</div>
 						</li>
-					)
+					);
 				})}
 			</ul>
 		</div>
